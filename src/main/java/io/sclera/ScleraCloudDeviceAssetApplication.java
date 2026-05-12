3473d2c4b78ddbfdcc97f59fb1e2435cc0ad9eed
dhanush-vk-sclera <backend.amp@sclera.com>
2026-05-12 14:07:19 +0530

    feat: add ScleraCloudDeviceAssetApplication entry point

---
+++ src/main/java/io/sclera/ScleraCloudDeviceAssetApplication.java
@@ -0,0 +1,12 @@
+﻿package io.sclera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScleraCloudDeviceAssetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScleraCloudDeviceAssetApplication.class, args);
+    }
+}
